4f684a8f4ab8887a184fc33bf423e255a82972bd
MamunUrRashidAIUB <md.mamun.ur.rashid.cse@gmail.com>
2025-07-29 20:01:20 +0600

    take all the button reference to main

--- app/src/main/java/com/example/calculatorapp/MainActivity.kt
+++ app/src/main/java/com/example/calculatorapp/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.example.calculatorapp
 
 import android.os.Bundle
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -16,5 +17,26 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+        //reference to the button
+        val button=findViewById<Button>(R.id.button4)
+        val button2=findViewById<Button>(R.id.button3)
+        val button3=findViewById<Button>(R.id.button2)
+        val button4=findViewById<Button>(R.id.button)
+        val button5=findViewById<Button>(R.id.button5)
+        val button6=findViewById<Button>(R.id.button6)
+        val button7=findViewById<Button>(R.id.button7)
+        val button8=findViewById<Button>(R.id.button8)
+        val button9=findViewById<Button>(R.id.button9)
+        val button10=findViewById<Button>(R.id.button10)
+        val button11=findViewById<Button>(R.id.button11)
+        val button12=findViewById<Button>(R.id.button12)
+        val button13=findViewById<Button>(R.id.button13)
+        val button14=findViewById<Button>(R.id.button14)
+        val button15=findViewById<Button>(R.id.button15)
+        val button16=findViewById<Button>(R.id.button16)
+
+
+
+
     }
 }
